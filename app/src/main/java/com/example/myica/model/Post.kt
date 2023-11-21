43f87c1c65944a0b1d9e84bfbfb824868dd026a8
Vamshi <W9622660@live.tees.ac.uk>
2023-11-21 12:42:35 +0000

    Updating the application - delete

--- app/src/main/java/com/example/myica/model/Post.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myica.model
-
-data class Post(
-    var id: String? = null,
-    var title: String? = null,
-    var content: String? = null,
-    var color: Int? = null,
-    var created_at: String? = null,
-)
--- app/src/main/java/com/example/myica/theme/Color.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myica.theme
-
-import androidx.compose.ui.graphics.Color
-
-val Purple200 = Color(0xC686FCD7)
-val Purple500 = Color(0xFF00BEEE)
-val Purple700 = Color(0xFFC3B8DB)
-val Teal200 = Color(0xFF03DAC5)
