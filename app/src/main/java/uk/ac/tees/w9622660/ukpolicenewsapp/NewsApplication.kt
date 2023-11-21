694c69cb03fab0d18b569d395e57fbdab23a8716
Vamshi <W9622660@live.tees.ac.uk>
2023-11-21 16:57:34 +0000

    Committed api

--- app/src/main/java/uk/ac/tees/w9622660/ukpolicenewsapp/NewsApplication.kt
+++ app/src/main/java/uk/ac/tees/w9622660/ukpolicenewsapp/NewsApplication.kt
@@ -2,6 +2,7 @@ package uk.ac.tees.w9622660.ukpolicenewsapp
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import kotlin.text.Typography.dagger
 
 @HiltAndroidApp
 class NewsApplication : Application() {
